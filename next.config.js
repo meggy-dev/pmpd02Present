7e98102b27aae8d57b25a8ae0f44c5b8edade80c
meggy-dev <mollie.englefield@gmail.com>
2026-03-02 09:15:17 +0000

    github upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/pmpd02Present',        // Replace with your actual GitHub repo name
+  assetPrefix: '/pmpd02Present/',    // Same as above
+};
+
+module.exports = nextConfig;
