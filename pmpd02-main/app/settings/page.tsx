a215b53670b9ff430db1587588c6223d6cf4d5f4
meggy-dev <mollie.englefield@gmail.com>
2026-03-02 09:23:42 +0000

    settings change

--- pmpd02-main/app/settings/page.tsx
+++ pmpd02-main/app/settings/page.tsx
@@ -183,7 +183,7 @@ export default function Page() {
             </button>
           </div>
 
-          {/* Language */}
+          
           <Dropdown name="Language">
             <section>
               <Toggle
